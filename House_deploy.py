2ac0354be1afba9de1b01a3cd498099c39b7780c
sayalilakade2 <156512833+sayalilakade2@users.noreply.github.com>
2025-09-06 16:15:56 +0530

    Update House_deploy.py

--- House_deploy.py
+++ House_deploy.py
@@ -16,7 +16,7 @@ import joblib
 # Load the model
 #loaded_model = pickle.load(open(r"https://github.com/sayalilakade2/House_deploy-app/raw/main/finalized_model.sav", 'rb'))
 # Load the model
-model_url = "https://github.com/sayalilakade2/House_deploy-app/raw/main/finalized_model.sav"
+model_url = "https://github.com/sayalilakade2/House_Price_Prediction/blob/main/finalized_model.sav"
 r = requests.get(model_url)
 
 if r.status_code == 200:
@@ -64,4 +64,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
